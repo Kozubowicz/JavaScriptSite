491a943c72227b0908ef41b828628e71e150971b
Kozubowicz <55114447+Kozubowicz@users.noreply.github.com>
2023-06-06 22:54:21 +0200

    small fix6

--- public/Projects/Project 10 Blog/public/Posts/Template/script.js
+++ public/Projects/Project 10 Blog/public/Posts/Template/script.js
@@ -1,11 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
   var calendarEl = document.getElementById("calendar");
   var calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: "dayGridMonth",
+    headerToolbar: {
+      start: "prev,next,title", // will normally be on the left. if RTL, will be on the right
+      center: "", // will normally be on the right. if RTL, will be on the left
+      end: "",
+    },
+    titleFormat: { year: "numeric", month: "numeric" },
+
     contentHeight: 450,
-    editable: true,
-    selectable: true,
-    businessHours: true,
+    showNonCurrentDates: false,
   });
   calendar.render();
 });
